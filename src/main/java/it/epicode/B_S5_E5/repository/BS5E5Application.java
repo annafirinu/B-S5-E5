6f1426659ff2bd1370b5c622aa7db8efc86ba89a
annafirinu <anna.firinu2@outlook.it>
2025-06-06 10:50:50 +0200

    Initial commit

---
+++ src/main/java/it/epicode/B_S5_E5/repository/BS5E5Application.java
@@ -0,0 +1,13 @@
+package it.epicode.B_S5_E5.repository;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BS5E5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BS5E5Application.class, args);
+	}
+
+}
---
+++ src/test/java/it/epicode/B_S5_E5/BS5E5ApplicationTests.java
@@ -0,0 +1,13 @@
+package it.epicode.B_S5_E5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BS5E5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
